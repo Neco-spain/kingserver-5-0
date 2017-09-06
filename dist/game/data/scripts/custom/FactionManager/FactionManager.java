435a88bbd1441d760830dac0a0e92dda94a06d22
destrodevianne <destrodevianne@73c8beaf-0c33-4e40-9e04-903dee28e062>
2017-09-06 04:45:50 +0000

    Commit inicial

---
+++ dist/game/data/scripts/custom/FactionManager/FactionManager.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright (C) 2004-2017 L2J DataPack
+ * 
+ * This file is part of L2J DataPack.
+ * 
+ * L2J DataPack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * L2J DataPack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package custom.FactionManager;
+
+import com.l2jserver.Config;
+import com.l2jserver.gameserver.model.L2World;
+import com.l2jserver.gameserver.model.Location;
+import com.l2jserver.gameserver.model.actor.L2Npc;
+import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
+import com.l2jserver.gameserver.network.clientpackets.Say2;
+import com.l2jserver.gameserver.network.serverpackets.NpcHtmlMessage;
+
+import ai.npc.AbstractNpcAI;
+
+/**
+ * @author Destro
+ */
+
+public class FactionManager extends AbstractNpcAI
+{
+	// NPC
+	private static final int MANAGER = Config.FACTION_MANAGER_NPCID;
+	private static final int GOOD_GUARD = Config.FACTION_GOOD_GUARD_NPCID;
+	private static final int EVIL_GUARD = Config.FACTION_EVIL_GUARD_NPCID;
+	// Other
+	private static final String[] TEXTS =
+	{
+		Config.FACTION_GOOD_TEAM_NAME + " or " + Config.FACTION_EVIL_TEAM_NAME + "?",
+		"Select your faction!",
+		"The choice is yours!"
+	};
+	
+	private FactionManager()
+	{
+		super(FactionManager.class.getSimpleName(), "custom");
+		addSpawnId(MANAGER);
+		addStartNpc(MANAGER);
+		addTalkId(MANAGER);
+		addFirstTalkId(MANAGER);
+		addAggroRangeEnterId(EVIL_GUARD, GOOD_GUARD);
+		
+		Location locManager = new Location(Config.SPAWN_X_FACTION_MANAGER_LOCATION, Config.SPAWN_Y_FACTION_MANAGER_LOCATION, Config.SPAWN_Z_FACTION_MANAGER_LOCATION, Config.SPAWN_H_FACTION_MANAGER_LOCATION);
+		if (Config.FACTION_SYSTEM_ENABLED)
+		{
+			addSpawn(MANAGER, locManager, false, 0);
+		}
+	}
+	
+	@Override
+	public String onAdvEvent(String event, L2Npc npc, L2PcInstance player)
+	{
+		Location locGood = new Location(Config.SPAWN_X_FACTION_GOOD_BASE_LOCATION, Config.SPAWN_Y_FACTION_GOOD_BASE_LOCATION, Config.SPAWN_Z_FACTION_GOOD_BASE_LOCATION);
+		Location locEvil = new Location(Config.SPAWN_X_FACTION_EVIL_BASE_LOCATION, Config.SPAWN_Y_FACTION_EVIL_BASE_LOCATION, Config.SPAWN_Z_FACTION_EVIL_BASE_LOCATION);
+		switch (event)
+		{
+			case "selectGoodFaction":
+			{
+				if (Config.FACTION_BALANCE_ONLINE_PLAYERS && (L2World.getInstance().getAllGoodPlayersCount() >= ((L2World.getInstance().getAllEvilPlayersCount() + Config.FACTION_BALANCE_PLAYER_EXCEED_LIMIT))))
+				{
+					String htmltext = null;
+					final NpcHtmlMessage packet = new NpcHtmlMessage(npc.getObjectId());
+					packet.setHtml(getHtm(player.getHtmlPrefix(), "onlinelimit.html"));
+					packet.replace("%name%", player.getName());
+					packet.replace("%more%", Config.FACTION_GOOD_TEAM_NAME);
+					packet.replace("%less%", Config.FACTION_EVIL_TEAM_NAME);
+					player.sendPacket(packet);
+					return htmltext;
+				}
+				if (Config.FACTION_AUTO_NOBLESS)
+				{
+					player.setNoble(true);
+				}
+				if (Config.FACTION_AUTO_HERO)
+				{
+					player.setHero(true);
+				}
+				player.setGood();
+				player.getAppearance().setNameColor(Config.FACTION_GOOD_NAME_COLOR);
+				player.getAppearance().setTitleColor(Config.FACTION_GOOD_NAME_COLOR);
+				player.setTitle(Config.FACTION_GOOD_TEAM_NAME);
+				player.sendMessage("You are now fighting for the " + Config.FACTION_GOOD_TEAM_NAME + " faction.");
+				player.teleToLocation(locGood);
+				broadcastMessageToFaction(Config.FACTION_GOOD_TEAM_NAME, Config.FACTION_GOOD_TEAM_NAME + " faction grows stronger with the arrival of " + player.getName() + ".");
+				L2World.addFactionPlayerToWorld(player);
+				break;
+			}
+			case "selectEvilFaction":
+			{
+				if (Config.FACTION_BALANCE_ONLINE_PLAYERS && (L2World.getInstance().getAllEvilPlayersCount() >= ((L2World.getInstance().getAllGoodPlayersCount() + Config.FACTION_BALANCE_PLAYER_EXCEED_LIMIT))))
+				{
+					String htmltext = null;
+					final NpcHtmlMessage packet = new NpcHtmlMessage(npc.getObjectId());
+					packet.setHtml(getHtm(player.getHtmlPrefix(), "onlinelimit.html"));
+					packet.replace("%name%", player.getName());
+					packet.replace("%more%", Config.FACTION_EVIL_TEAM_NAME);
+					packet.replace("%less%", Config.FACTION_GOOD_TEAM_NAME);
+					player.sendPacket(packet);
+					return htmltext;
+				}
+				if (Config.FACTION_AUTO_NOBLESS)
+				{
+					player.setNoble(true);
+				}
+				if (Config.FACTION_AUTO_HERO)
+				{
+					player.setHero(true);
+				}
+				player.setEvil();
+				player.getAppearance().setNameColor(Config.FACTION_EVIL_NAME_COLOR);
+				player.getAppearance().setTitleColor(Config.FACTION_EVIL_NAME_COLOR);
+				player.setTitle(Config.FACTION_EVIL_TEAM_NAME);
+				player.sendMessage("You are now fighting for the " + Config.FACTION_EVIL_TEAM_NAME + " faction.");
+				player.teleToLocation(locEvil);
+				broadcastMessageToFaction(Config.FACTION_EVIL_TEAM_NAME, Config.FACTION_EVIL_TEAM_NAME + " faction grows stronger with the arrival of " + player.getName() + ".");
+				L2World.addFactionPlayerToWorld(player);
+				break;
+			}
+			case "SPEAK":
+			{
+				if (npc != null)
+				{
+					broadcastNpcSay(npc, Say2.NPC_ALL, TEXTS[getRandom(TEXTS.length)]);
+				}
+				break;
+			}
+		}
+		return super.onAdvEvent(event, npc, player);
+	}
+	
+	@Override
+	public String onFirstTalk(L2Npc npc, L2PcInstance player)
+	{
+		String htmltext = null;
+		final NpcHtmlMessage packet = new NpcHtmlMessage(npc.getObjectId());
+		packet.setHtml(getHtm(player.getHtmlPrefix(), "manager.html"));
+		packet.replace("%name%", player.getName());
+		packet.replace("%good%", Config.FACTION_GOOD_TEAM_NAME);
+		packet.replace("%evil%", Config.FACTION_EVIL_TEAM_NAME);
+		player.sendPacket(packet);
+		return htmltext;
+	}
+	
+	@Override
+	public String onSpawn(L2Npc npc)
+	{
+		startQuestTimer("SPEAK", 10000, npc, null, true);
+		return super.onSpawn(npc);
+	}
+	
+	private void broadcastMessageToFaction(String factionName, String message)
+	{
+		if (factionName == Config.FACTION_GOOD_TEAM_NAME)
+		{
+			for (L2PcInstance player : L2World.getInstance().getAllGoodPlayers())
+			{
+				player.sendMessage(message);
+			}
+		}
+		else
+		{
+			for (L2PcInstance player : L2World.getInstance().getAllEvilPlayers())
+			{
+				player.sendMessage(message);
+			}
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		new FactionManager();
+	}
+}
---
+++ dist/game/data/scripts/handlers/chathandlers/ChatTrade.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2004-2016 L2J DataPack
+ * 
+ * This file is part of L2J DataPack.
+ * 
+ * L2J DataPack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * L2J DataPack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package handlers.chathandlers;
+
+import com.l2jserver.Config;
+import com.l2jserver.gameserver.handler.IChatHandler;
+import com.l2jserver.gameserver.instancemanager.MapRegionManager;
+import com.l2jserver.gameserver.model.BlockList;
+import com.l2jserver.gameserver.model.L2World;
+import com.l2jserver.gameserver.model.PcCondOverride;
+import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
+import com.l2jserver.gameserver.network.SystemMessageId;
+import com.l2jserver.gameserver.network.serverpackets.CreatureSay;
+import com.l2jserver.gameserver.util.Util;
+
+import ZeuS.ZeuS.ZeuS;
+
+/**
+ * Trade chat handler.
+ * @author durgus
+ */
+public class ChatTrade implements IChatHandler
+{
+	private static final int[] COMMAND_IDS =
+	{
+		8
+	};
+	
+	/**
+	 * Handle chat type 'trade'
+	 */
+	@Override
+	public void handleChat(int type, L2PcInstance activeChar, String target, String text)
+	{
+		if (activeChar.isChatBanned() && Util.contains(Config.BAN_CHAT_CHANNELS, type))
+		{
+			activeChar.sendPacket(SystemMessageId.CHATTING_IS_CURRENTLY_PROHIBITED);
+			return;
+		}
+		// zeus --------------------------------------------------------------------------------
+		if (!ZeuS.canChatTrade(activeChar))
+		{
+			return;
+		}
+		// -------------------------------------------------------------------------------------
+		final CreatureSay cs = new CreatureSay(activeChar.getObjectId(), type, activeChar.getName(), text);
+		if (Config.DEFAULT_TRADE_CHAT.equalsIgnoreCase("on") || (Config.DEFAULT_TRADE_CHAT.equalsIgnoreCase("gm") && activeChar.canOverrideCond(PcCondOverride.CHAT_CONDITIONS)))
+		{
+			int region = MapRegionManager.getInstance().getMapRegionLocId(activeChar);
+			for (L2PcInstance player : L2World.getInstance().getPlayers())
+			{
+				if ((region == MapRegionManager.getInstance().getMapRegionLocId(player)) && !BlockList.isBlocked(player, activeChar) && (player.getInstanceId() == activeChar.getInstanceId()))
+				{
+					if (Config.FACTION_SYSTEM_ENABLED)
+					{
+						if (Config.FACTION_SPECIFIC_CHAT)
+						{
+							if ((activeChar.isGood() && player.isGood()) || (activeChar.isEvil() && player.isEvil()))
+							{
+								player.sendPacket(cs);
+							}
+						}
+						else
+						{
+							player.sendPacket(cs);
+						}
+					}
+					else
+					{
+						player.sendPacket(cs);
+					}
+				}
+			}
+		}
+		else if (Config.DEFAULT_TRADE_CHAT.equalsIgnoreCase("global"))
+		{
+			if (!activeChar.canOverrideCond(PcCondOverride.CHAT_CONDITIONS) && !activeChar.getFloodProtectors().getGlobalChat().tryPerformAction("global chat"))
+			{
+				activeChar.sendMessage("Do not spam trade channel.");
+				return;
+			}
+			
+			for (L2PcInstance player : L2World.getInstance().getPlayers())
+			{
+				if (!BlockList.isBlocked(player, activeChar))
+				{
+					if (Config.FACTION_SYSTEM_ENABLED)
+					{
+						if (Config.FACTION_SPECIFIC_CHAT)
+						{
+							if ((activeChar.isGood() && player.isGood()) || (activeChar.isEvil() && player.isEvil()))
+							{
+								player.sendPacket(cs);
+							}
+						}
+						else
+						{
+							player.sendPacket(cs);
+						}
+					}
+					else
+					{
+						player.sendPacket(cs);
+					}
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Returns the chat types registered to this handler.
+	 */
+	@Override
+	public int[] getChatTypeList()
+	{
+		return COMMAND_IDS;
+	}
+}
